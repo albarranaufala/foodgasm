9f842bfecf2a5fc25dbac118cf3236d4c72c2a8a
Albarra Erdanto <albarraerdanto@gmail.com>
2021-06-19 21:13:49 +0700

    add cacheAPI

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,6 +3,7 @@ const CONFIG = {
   BASE_SMALL_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/small',
   BASE_MEDIUM_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium',
   BASE_LARGE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/large',
+  CACHE_NAME: new Date().toISOString(),
 };
 
 export default CONFIG;
