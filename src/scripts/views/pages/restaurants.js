1a49d57a44fc1f5d48ff9d6de5964b99db8dd1b5
Albarra Erdanto <albarraerdanto@gmail.com>
2021-06-19 15:26:02 +0700

    add restaurant page

--- src/scripts/views/pages/restaurants.js
+++ src/scripts/views/pages/restaurants.js
@@ -1,12 +1,43 @@
+import RestaurantsSource from "../../data/restaurants-source";
+import { createRestaurantItemTemplate } from "../templates/template-creator";
+
 const Restaurants = {
   async render() {
     return `
-      <h2>Restaurants</h2>
+        <section>
+          <article id="hero">
+              <div class="container">
+                  <div class="left">
+                      <h1>
+                          All will be <span class="underline">foodgasm</span> in time
+                      </h1>
+                      <a href="#restaurants">
+                          Let's Eat!
+                      </a>
+                  </div>
+              </div>
+          </article>
+          <div class="desktop-down down">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 13l-7 7-7-7m14-8l-7 7-7-7" />
+              </svg>
+          </div>
+          <article id="restaurants" class="container">
+              <h2>
+                  Beloved Restaurants
+              </h2>
+              <div id="restaurantsContainer" class="restaurants-wrapper" />
+          </article>
+      </section>
     `;
   },
  
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const restaurants = await RestaurantsSource.getAll();
+    const restaurantsContainer = document.getElementById('restaurantsContainer');
+    restaurantsContainer.innerHTML = restaurants
+      .map((restaurant) => createRestaurantItemTemplate(restaurant))
+      .reduce((sum, restaurant) => sum + restaurant);
   },
 };
  
